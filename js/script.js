0ccb31e672c09f9153b3cb501d15281f8a1eb083
Diana-Diana-dev <dianaargunina@gmail.com>
2021-10-17 00:47:00 +0300

    feat: add footer and time date weather

--- js/script.js
+++ js/script.js
@@ -35,5 +35,57 @@ function showSlides(n) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    // dots[slideIndex - 1].className += " active";
 }
+
+
+
+
+ /* функция добавления ведущих нулей */
+    /* (если число меньше десяти, перед числом добавляем ноль) */
+    function zero_first_format(value)
+    {
+        if (value < 10)
+        {
+            value='0'+value;
+        }
+        return value;
+    }
+
+    /* функция получения текущей даты и времени */
+    function time()
+    {
+        var current_datetime = new Date();
+        var day = zero_first_format(current_datetime.getDate());
+        var month = zero_first_format(current_datetime.getMonth()+1);
+        var year = current_datetime.getFullYear();
+        var hours = zero_first_format(current_datetime.getHours());
+        var minutes = zero_first_format(current_datetime.getMinutes());
+        var seconds = zero_first_format(current_datetime.getSeconds());
+
+        // return day+"."+month+"."+year+" "+hours+":"+minutes+":"+seconds;
+        return hours+":"+minutes;
+    }
+
+    function date()
+    {
+        var monthwords=new Array("января","февраля","марта","апреля","мая","июня",
+"июля","августа","сентября","октября","ноября","декабря");
+        var current_datetime = new Date();
+        var day = zero_first_format(current_datetime.getDate());
+        var month = monthwords[current_datetime.getMonth()];
+        var year = current_datetime.getFullYear();
+        var hours = zero_first_format(current_datetime.getHours());
+        var minutes = zero_first_format(current_datetime.getMinutes());
+        var seconds = zero_first_format(current_datetime.getSeconds());
+
+        return day+" "+month+" "+year;
+       
+    }
+
+    /* выводим текущую дату и время на сайт в блок с id "current_date_time_block" */
+    setInterval(function () {
+        document.getElementById('current_time_block').innerHTML = time();
+    }, 1000);
+    
+        document.getElementById('current_date_block').innerHTML = date();
